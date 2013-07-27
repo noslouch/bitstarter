83b1d8bc227d175f97518d5e145027e41bbefe61
ec2 box <me@brianwhitton.com>
2013-07-27 02:58:14 +0000

    update wb.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var index = fs.readFileSync('index.html',{encoding: "utf-"});
+    var index = fs.readFileSync('index.html',{encoding: "utf-8"});
     response.send(index);
 });
 
