f991a342f606681ca5909bc967617e0fa7319796
ec2 box <me@brianwhitton.com>
2013-07-27 02:56:57 +0000

    update wb.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var index = fs.readFileSync('index.html',{encoding: "utf-"});
+    response.send(index);
 });
 
 var port = process.env.PORT || 5000;
