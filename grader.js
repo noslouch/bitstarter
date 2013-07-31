8d60b0e64f8c666972673337a6a78d169b0f440f
ec2 box <me@brianwhitton.com>
2013-07-31 04:05:43 +0000

    finished homework

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs')
 var program = require('commander')
 var cheerio = require('cheerio')
+var restler = require('restler')
 var HTMLFILE_DEFAULT = 'index.html'
 var CHECKSFILE_DEFAULT =  'checks.json'
 
@@ -13,6 +14,18 @@ var assertFileExists = function(infile){
         process.exit(1)
     }
     return instr
+}
+
+var assertUrlExists = function(url){
+    var res;
+    restler.get(url).on('complete', function(result, response){
+        res = result
+    })
+    if (typeof res === 'string') {
+        console.log('%s does not exist. Exiting.', url)
+        process.exit(1)
+    }
+    return res
 }
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -42,8 +55,9 @@ if (require.main == module){
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_address>', 'URL to web document', clone(assertUrlExists))
         .parse(process.argv)
-    var checkJson = checkHtmlFile(program.file, program.checks)
+    var checkJson = checkHtmlFile(program.file || program.url, program.checks)
     var outJson = JSON.stringify(checkJson, null, 4)
     console.log(outJson)
 } else {
